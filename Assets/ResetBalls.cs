382c6720f99c855cb76ddface451cf63e07f548d
TheAlexet <thealexet6@gmail.com>
2021-03-19 00:22:44 +0100

    Deliverable 2 0.7

--- Assets/ResetBalls.cs
+++ Assets/ResetBalls.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResetBalls : MonoBehaviour
 {
@@ -10,28 +11,32 @@ public class ResetBalls : MonoBehaviour
     public GameObject ball3;
     public GameObject ball4;
     public GameObject ball5;
+    public Text scoreLabel;
 
-    public void resetButtonHandler() {
+    public void resetButtonHandler() 
+    {
 
         ball1.SetActive(false);
         ball1.transform.position = new Vector3(-4.985f, 4.32f, 0f);
         ball1.SetActive(true);
 
         ball2.SetActive(false);
-        ball2.transform.position = new Vector3(-0.281f, 0.661f, 4.713f);
+        ball2.transform.position = new Vector3(-4.985f, 4.4f, 0.9f);
         ball2.SetActive(true);
 
         ball3.SetActive(false);
-        ball3.transform.position = new Vector3(-4.985f, 4.32f, 0f);
+        ball3.transform.position = new Vector3(-4.985f, 4.6f, 1.7f);
         ball3.SetActive(true);
 
         ball4.SetActive(false);
-        ball4.transform.position = new Vector3(-4.985f, 4.32f, 0f);
+        ball4.transform.position = new Vector3(-4.985f, 4.65f, 2.4f);
         ball4.SetActive(true);
 
         ball5.SetActive(false);
-        ball5.transform.position = new Vector3(-4.985f, 4.32f, 0f);
+        ball5.transform.position = new Vector3(-4.985f, 4.7f, 3f);
         ball5.SetActive(true);
+
+        scoreLabel.GetComponent<Text>().text = "Score: 0";
 
     }
 
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+	public Text scoreLabel;
+
+	private int scoreCount;
+
+	void Start()
+	{
+		scoreCount = 0;
+		scoreLabel.GetComponent<Text>().text = "Score: " + scoreCount.ToString();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+
+		if (col.tag == "Player") 
+		{
+			scoreCount = scoreCount + 1;
+			scoreLabel.GetComponent<Text>().text = "Score: " + scoreCount.ToString();
+		}
+
+	}
+
+}

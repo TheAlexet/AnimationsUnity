f835de3a08535e837d0fd04f219b6f2042d38a82
TheAlexet <thealexet6@gmail.com>
2021-04-30 19:31:34 +0200

    Portfolio 8

---
+++ Assets/Portfolio 8/Mover_IA.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover_IA : MonoBehaviour
+{
+
+    public Transform goal;
+    NavMeshAgent agent;
+    Animator animatorPersonaje;
+    public Camera camara;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animatorPersonaje = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camara.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+
+        /*if (agent.isOnOffMeshLink) 
+        {
+            animatorPersonaje.SetTrigger("Saltar");
+        }*/
+
+        animatorPersonaje.SetFloat("horizontal", transform.InverseTransformDirection(agent.velocity).x);
+        animatorPersonaje.SetFloat("vertical", transform.InverseTransformDirection(agent.velocity).z);
+    }
+}
